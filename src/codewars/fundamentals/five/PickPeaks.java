3315c666032a2dffd15d4697b4e0bc23c9f0cffd
sychsergiy <sychsergiy@gmail.com>
2020-11-06 17:32:16 +0200

    update PickPeaks solution

--- src/codewars/fundamentals/five/PickPeaks.java
+++ src/codewars/fundamentals/five/PickPeaks.java
@@ -1,162 +1,76 @@
 package codewars.fundamentals.five;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+//  https://www.codewars.com/kata/5279f6fe5ab7f447890006a7
 
 public class PickPeaks {
     private static class Point {
+        private final int position;
+        private final int value;
+
         Point(int position, int value) {
             this.position = position;
             this.value = value;
         }
 
-        public int position;
-        public int value;
-    }
-
-    private static class Analyzer {
-
-        private static class PlateauState {
-            private List<Point> points = new ArrayList<>();
-
-            public void resetIfNotEmpty() {
-                if (!points.isEmpty()) {
-                    points = new ArrayList<>();
-                }
-            }
-
-            public void addPoint(Point p) {
-                points.add(p);
-            }
-
-            public Point getFirstPoint() {
-                return points.get(0);
-            }
-
-            public boolean isNotEmpty() {
-                return !points.isEmpty();
-            }
+        public int getValue() {
+            return value;
         }
 
-        private enum State {RISE, DOWN}
-
-        private Point lastPoint;
-        private List<Point> maximums = new ArrayList<>();
-        private List<Point> minimums = new ArrayList<>();
-        private State state;
-        private PlateauState plateauState = new PlateauState();
-
-        public List<Point> getMaximums() {
-            return this.maximums;
-        }
-
-        public List<Point> getMinimums() {
-            return this.minimums;
-        }
-
-        Analyzer(int currentNumber) {
-            this.lastPoint = new Point(0, currentNumber);
-            this.state = State.DOWN; // set DOWN as initial state to ignore maximum at the edge
-        }
-
-        private void updateLastPoint(int value) {
-            this.lastPoint = new Point(lastPoint.position + 1, value);
-
-        }
-
-        public void readNextNumber(int number) {
-            switch (this.state) {
-                case DOWN:
-                    if (number > this.lastPoint.value) {
-                        // given number is local minimum
-
-                        // check if last points was a plateau
-                        // take first point of plateau if yes
-                        if (plateauState.isNotEmpty()) {
-                            this.minimums.add(plateauState.getFirstPoint());
-                        } else {
-                            this.minimums.add(this.lastPoint);
-                        }
-
-                        this.state = State.RISE;
-                        updateLastPoint(number);
-                        plateauState.resetIfNotEmpty();
-                    } else if (number == this.lastPoint.value) {
-                        plateauState.addPoint(this.lastPoint);
-                        updateLastPoint(number);
-                        // given number is plateau
-                        // skip currentNumber update to save first number from plateau as minimum or maximum
-                    } else {
-                        plateauState.resetIfNotEmpty();
-                        updateLastPoint(number);
-                    }
-                    break;
-                case RISE:
-                    if (number > this.lastPoint.value) {
-                        updateLastPoint(number);
-                        plateauState.resetIfNotEmpty();
-                    } else if (number == this.lastPoint.value) {
-                        plateauState.addPoint(this.lastPoint);
-                        updateLastPoint(number);
-                        // given number is plateau
-                        // keep position to save first number from plateau as minimum or maximum
-                    } else {
-                        // given number is local maximum;
-
-                        // check if last points was a plateau
-                        // take first point of plateau if yes
-                        if (plateauState.isNotEmpty()) {
-                            this.maximums.add(plateauState.getFirstPoint());
-                        } else {
-                            this.maximums.add(this.lastPoint);
-                        }
-                        updateLastPoint(number);
-                        this.state = State.DOWN;
-                        plateauState.resetIfNotEmpty();
-                    }
-                    break;
-            }
+        public int getPosition() {
+            return position;
         }
     }
-
-    private static class PeaksMap {
-        private Map<String, List<Integer>> peaksMap;
-
-        PeaksMap() {
-            this.peaksMap = new HashMap<>();
-            this.peaksMap.put("pos", new ArrayList<>());
-            this.peaksMap.put("peaks", new ArrayList<>());
-        }
-
-        public void add(Point p) {
-            this.peaksMap.get("pos").add(p.position);
-            this.peaksMap.get("peaks").add(p.value);
-        }
-
-        public Map<String, List<Integer>> get() {
-            return this.peaksMap;
-        }
-    }
-
 
     public static Map<String, List<Integer>> getPeaks(int[] arr) {
-        PeaksMap map = new PeaksMap();
 
-        if (arr.length == 0){
-            return map.get();
+        // declare func is asc or desc at current point flag
+        boolean ascending = false;
+
+        // declare start of plateau
+        Optional<Point> plateauStart = Optional.empty();
+
+        // for each item in input arr:
+        // check if maximum:
+        //      if (next item is smaller than current)  and (func is asc):
+        //          if (start of plateau) :
+        //              1) set func state flag to desc
+        //              2) save start of plateau as maximum
+        //              3) reset start of plateau
+        //          else:
+        //              1) save current item as maximum
+        //      elif (next item is bigger than current):
+        //          1) set func state flag to asc
+        //          2) reset start of plateau
+        //      elif (next item == current):
+        //          if (start of plateau): skip
+        //
+
+        List<Point> maximums = new ArrayList<>();
+        for (int i = 0; i < arr.length - 1; i++) {
+            if ((arr[i + 1] < arr[i]) && ascending) {
+                if (plateauStart.isPresent()) {
+                    maximums.add(plateauStart.get());
+                    plateauStart = Optional.empty();
+                } else {
+                    maximums.add(new Point(i, arr[i]));
+                }
+                ascending = false;
+            } else if (arr[i + 1] > arr[i]) {
+                ascending = true;
+                plateauStart = Optional.empty();
+            } else {
+                if (plateauStart.isEmpty()) {
+                    plateauStart = Optional.of(new Point(i, arr[i]));
+                }
+            }
         }
 
-        Analyzer analyzer = new Analyzer(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            analyzer.readNextNumber(arr[i]);
-        }
-
-        for (Point p : analyzer.getMaximums()) {
-            map.add(p);
-        }
-        return map.get();
+        return new HashMap<>() {{
+            put("peaks", maximums.stream().map(p -> p.value).collect(Collectors.toList()));
+            put("pos", maximums.stream().map(p -> p.position).collect(Collectors.toList()));
+        }};
     }
 }
